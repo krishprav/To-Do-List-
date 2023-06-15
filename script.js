bbe616c4db27519c19330bd844fd166884e7b153
krishprav <118839797+krishprav@users.noreply.github.com>
2023-06-15 16:51:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
+const deadlineInput = document.querySelector(".deadline-input");
+const quoteElement = document.querySelector(".quote");
+const completeSound = document.querySelector(".complete-sound");
 
 document.addEventListener("DOMContentLoaded", getLocalTodos);
 todoButton.addEventListener("click", addTodo);
@@ -16,58 +19,68 @@ function addTodo(event) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
-    //ADDING TO LOCAL STORAGE
+    // ADDING TO LOCAL STORAGE
     saveLocalTodos(todoInput.value);
 
     const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check-circle"></li>';
+    completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
 
     const trashButton = document.createElement("button");
-    trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
     trashButton.classList.add("trash-btn");
     todoDiv.appendChild(trashButton);
 
+    const deadlineTime = document.createElement("p");
+    deadlineTime.innerText = formatDeadline(deadlineInput.value);
+    deadlineTime.classList.add("deadline-time");
+    todoDiv.appendChild(deadlineTime);
+
     todoList.appendChild(todoDiv);
     todoInput.value = "";
+    deadlineInput.value = "";
+
+    playCompleteSound();
+    displayRandomQuote();
 }
 
 function deleteCheck(e) {
     const item = e.target;
 
-    if(item.classList[0] === "trash-btn") {
+    if (item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
         todo.classList.add("slide");
 
         removeLocalTodos(todo);
-        todo.addEventListener("transitionend", function() {
+        todo.addEventListener("transitionend", function () {
             todo.remove();
         });
     }
 
-    if(item.classList[0] === "complete-btn") {
+    if (item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
+        playCompleteSound();
     }
 }
 
-function filterTodo(e) {
+function filterTodo() {
     const todos = todoList.childNodes;
-    todos.forEach(function(todo) {
-        switch(e.target.value) {
+    todos.forEach(function (todo) {
+        switch (filterOption.value) {
             case "all":
                 todo.style.display = "flex";
                 break;
             case "completed":
-                if(todo.classList.contains("completed")) {
+                if (todo.classList.contains("completed")) {
                     todo.style.display = "flex";
                 } else {
                     todo.style.display = "none";
                 }
                 break;
             case "incomplete":
-                if(!todo.classList.contains("completed")) {
+                if (!todo.classList.contains("completed")) {
                     todo.style.display = "flex";
                 } else {
                     todo.style.display = "none";
@@ -79,7 +92,7 @@ function filterTodo(e) {
 
 function saveLocalTodos(todo) {
     let todos;
-    if(localStorage.getItem("todos") === null) {
+    if (localStorage.getItem("todos") === null) {
         todos = [];
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
@@ -90,12 +103,12 @@ function saveLocalTodos(todo) {
 
 function getLocalTodos() {
     let todos;
-    if(localStorage.getItem("todos") === null) {
+    if (localStorage.getItem("todos") === null) {
         todos = [];
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
-    todos.forEach(function(todo) {
+    todos.forEach(function (todo) {
         const todoDiv = document.createElement("div");
         todoDiv.classList.add("todo");
         const newTodo = document.createElement("li");
@@ -104,14 +117,19 @@ function getLocalTodos() {
         todoDiv.appendChild(newTodo);
 
         const completedButton = document.createElement("button");
-        completedButton.innerHTML = '<i class="fas fa-check-circle"></li>';
+        completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
         completedButton.classList.add("complete-btn");
         todoDiv.appendChild(completedButton);
 
         const trashButton = document.createElement("button");
-        trashButton.innerHTML = '<i class="fas fa-trash"></li>';
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
         trashButton.classList.add("trash-btn");
         todoDiv.appendChild(trashButton);
+
+        const deadlineTime = document.createElement("p");
+        deadlineTime.innerText = formatDeadline(deadlineInput.value);
+        deadlineTime.classList.add("deadline-time");
+        todoDiv.appendChild(deadlineTime);
 
         todoList.appendChild(todoDiv);
     });
@@ -119,7 +137,7 @@ function getLocalTodos() {
 
 function removeLocalTodos(todo) {
     let todos;
-    if(localStorage.getItem("todos") === null) {
+    if (localStorage.getItem("todos") === null) {
         todos = [];
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
@@ -128,4 +146,29 @@ function removeLocalTodos(todo) {
     const todoIndex = todo.children[0].innerText;
     todos.splice(todos.indexOf(todoIndex), 1);
     localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function formatDeadline(deadline) {
+    const date = new Date(deadline);
+    const options = { year: "numeric", month: "short", day: "numeric", hour: "numeric", minute: "numeric" };
+    return date.toLocaleString("en-US", options);
+}
+
+function playCompleteSound() {
+    completeSound.currentTime = 0;
+    completeSound.play();
+}
+
+function displayRandomQuote() {
+    const quotes = [
+        "The future depends on what you do today.",
+        "Believe you can and you're halfway there.",
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        "The only limit to our realization of tomorrow will be our doubts of today.",
+        "Don't watch the clock; do what it does. Keep going.",
+        "The secret of getting ahead is getting started.",
+        "You don't have to be great to start, but you have to start to be great."
+    ];
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteElement.textContent = quotes[randomIndex];
 }
